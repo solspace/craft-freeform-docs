941ec8e349c7b08b74757ff6596f230a26c356fb
thejahid <thejahid@icloud.com>
2024-05-21 20:48:11 +0600

    baseurl update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://emd.solspace.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/craft/freeform',
 
   // Static directory
   staticDirectories: ['static'],
