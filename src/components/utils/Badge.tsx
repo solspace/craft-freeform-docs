a51bae3dfe3605cb9a078951503bf1bb6f1fe360
thejahid <thejahid@icloud.com>
2024-06-03 20:03:23 +0600

    updage badge

--- src/components/utils/Badge.tsx
+++ src/components/utils/Badge.tsx
@@ -10,31 +10,19 @@ interface BadgeGroupProps {
 }
 
 const badgeStyles: { [key in BadgeType]: string } = {
-  lite: 'bg-gray-200 text-gray-800',
+  lite: 'bg-gradient-to-r from-amber-500 to-yellow-400 text-gray-800',
   pro: 'bg-gradient-to-r from-red-500 to-orange-500 text-white',
   feature: 'bg-gradient-to-r from-blue-800 to-violet-600 text-white',
   addon: 'bg-gradient-to-r from-amber-300 to-yellow-600 text-black',
   recommended: 'bg-gradient-to-r from-lime-700 to-lime-500 text-black',
 };
 
-const badgeIcons: { [key in BadgeType]?: string } = {
-  lite: '&#9734;', // star outline
-  pro: '&#9733;', // solid star
-  recommended: '&#128077;', // thumbs up
-};
-
 const Badge: React.FC<BadgeProps> = ({ type, text }) => {
   return (
     <span
-      className={`badge align-top !text-[10px] !font-bold mt-0.5 px-2 py-0.5 rounded-full ${badgeStyles[type]} ${type}`}
+      className={`badge align-top !text-[10px] !font-bold mt-0.5 px-2 py-1 rounded-full ${badgeStyles[type]} ${type}`}
     >
-      {badgeIcons[type] && (
-        <span
-          className="mr-1"
-          dangerouslySetInnerHTML={{ __html: badgeIcons[type] }}
-        />
-      )}
-      {text && <span>{text}</span>}
+      {text}
     </span>
   );
 };
