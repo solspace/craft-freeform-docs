9f72967348ed3ae51edbf8cc5f6716b4c6594498
thejahid <thejahid@icloud.com>
2024-06-06 16:46:03 +0600

    spam protection

--- src/components/utils/Photos.tsx
+++ src/components/utils/Photos.tsx
@@ -20,15 +20,17 @@ const ThemedIdealImage = (props: ThemedImageProps): JSX.Element => {
 
 const BrowserMockupWithPhoto = ({ src, alt }) => {
   return (
-    <div className="mx-auto mt-6">
+    <div className="flex flex-col mt-6">
       <div className="w-full h-11 rounded-t-lg bg-gray-200 dark:bg-gray-700 flex justify-start items-center space-x-1.5 px-3">
         <span className="w-3 h-3 rounded-full bg-red-400"></span>
         <span className="w-3 h-3 rounded-full bg-yellow-400"></span>
         <span className="w-3 h-3 rounded-full bg-green-400"></span>
       </div>
-      <div className="w-full m-0">
-        <Image className="rounded-b-lg" alt={alt} img={src} />
-      </div>
+      <Image
+        className="w-full m-0 overflow-hidden ideal-browser-mock"
+        alt={alt}
+        img={src}
+      />
     </div>
   );
 };
