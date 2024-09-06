8e8f4d4b0c0adfc24dc8c81da847e7f94dabf13a
thejahid <thejahid@icloud.com>
2024-09-06 23:36:08 +0600

    Freeform docs

---
+++ docusaurus.config.ts
@@ -0,0 +1,214 @@
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+import tailwind from './plugins/tailwind-config.cjs';
+import docusaurusGithubRemoteContent from './plugins/docusaurus-github-remote-content';
+
+import type { Options as IdealImageOptions } from '@docusaurus/plugin-ideal-image';
+
+import PrismLight from './src/utils/prismLight';
+import PrismDark from './src/utils/prismDark';
+
+const config: Config = {
+  title: 'Freeform for Craft',
+  tagline: 'Freeform is cool',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://docs.solspace.net/',
+  baseUrl: '/craft/freeform',
+
+  staticDirectories: ['static'],
+
+  organizationName: 'solspace',
+  projectName: 'freefrom-docs',
+
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    mermaid: true,
+  },
+
+  themes: ['@docusaurus/theme-mermaid'],
+
+  plugins: [
+    [
+      docusaurusGithubRemoteContent,
+      {
+        contents: [
+          {
+            version: 5,
+            saveDir: 'docs/setup',
+            file: 'changelog.mdx',
+            url: 'https://raw.githubusercontent.com/solspace/craft-freeform/v5/CHANGELOG.md',
+            meta: {
+              title: 'Changelog',
+              sidebar_label: 'Changelog',
+              sidebar_position: 5,
+            },
+          },
+          {
+            version: 4,
+            saveDir: 'versioned_docs/version-v4/setup',
+            file: 'changelog.mdx',
+            url: 'https://raw.githubusercontent.com/solspace/craft-freeform/v4/CHANGELOG.md',
+            meta: {
+              title: 'Changelog',
+              sidebar_label: 'Changelog',
+              sidebar_position: 5,
+            },
+          },
+          {
+            version: 3,
+            saveDir: 'versioned_docs/version-v3/setup',
+            file: 'changelog.mdx',
+            url: 'https://raw.githubusercontent.com/solspace/craft-freeform/v3/CHANGELOG.md',
+            meta: {
+              title: 'Changelog',
+              sidebar_label: 'Changelog',
+              sidebar_position: 5,
+            },
+          },
+          {
+            version: 2,
+            saveDir: 'versioned_docs/version-v2/setup',
+            file: 'changelog.mdx',
+            url: 'https://raw.githubusercontent.com/solspace/craft-freeform/v2/CHANGELOG.md',
+            meta: {
+              title: 'Changelog',
+              sidebar_label: 'Changelog',
+              sidebar_position: 5,
+            },
+          },
+        ],
+      },
+    ],
+    tailwind,
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 90,
+        max: 2040,
+        min: 640,
+        steps: 2,
+        disableInDev: false,
+      } satisfies IdealImageOptions,
+    ],
+  ],
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: '5.x',
+              path: 'v5',
+              badge: false,
+            },
+            v4: {
+              label: '4.x',
+              path: 'v4',
+              badge: false,
+            },
+            v3: {
+              label: '3.x',
+              path: 'v3',
+              badge: false,
+            },
+            v2: {
+              label: '2.x',
+              path: 'v2',
+              badge: false,
+            },
+            v1: {
+              label: '1.x',
+              path: 'v1',
+              badge: false,
+            },
+          },
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          admonitions: {
+            keywords: ['youtube', 'guide'],
+            extendDefaults: true,
+          },
+          editUrl:
+            'https://github.com/solspace/craft-freeform-docs/edit/develop',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    algolia: {
+      appId: '1DT1X6GS6A',
+      apiKey: '31f6b0738d7142090aea0e2191164eaa',
+      indexName: 'emd-solspace',
+      replaceSearchResultPathname: {
+        from: '/docs/',
+        to: '/',
+      },
+      searchParameters: {},
+      searchPagePath: 'search',
+      insights: false,
+    },
+
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Freeform',
+      logo: {
+        alt: 'Freeform',
+        src: 'img/freeform-icon.png',
+      },
+      items: [
+        {
+          type: 'docsVersionDropdown',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://plugins.craftcms.com/freeform',
+          label: 'Plugin Store',
+          position: 'right',
+        },
+        { to: '/v5/', label: 'Docs', position: 'right' },
+
+        { to: '/support', label: 'Support', position: 'right' },
+        {
+          type: 'search',
+          position: 'right',
+          className: 'navbar-search-icon',
+        },
+        {
+          type: 'html',
+          position: 'right',
+          className: 'github-icon',
+          value:
+            '<a href="https://github.com/solspace/craft-freeform/"><svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true" class="fill-gray-700 dark:fill-white h-6 w-6 mt-2"><path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path></svg></a>',
+        },
+      ],
+    },
+    prism: {
+      theme: PrismLight,
+      darkTheme: PrismDark,
+      additionalLanguages: [
+        'markup-templating',
+        'diff',
+        'php',
+        'yaml',
+        'nginx',
+        'json',
+        'nix',
+        'twig',
+      ],
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/pages/support/index.tsx
@@ -0,0 +1,77 @@
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+
+import { Footer } from '@site/src/components/layout';
+
+const Support: React.FC = () => {
+  return (
+    <Layout
+      title="Support"
+      description="Need help with Freeform? We love helping our customers discover best practices, resolve issues, and achieve their goals!"
+    >
+      <main className="max-w-5xl m-auto my-12">
+        <h1>Support</h1>
+        <p>
+          Need help with Freeform? We love helping our customers discover best
+          practices, resolve issues, and achieve their goals!
+        </p>
+        <p>
+          For more extensive or urgent needs, consider our
+          <Link href="/support/premium"> premium support services</Link>. Get
+          direct, high-priority access to our Software team for complex
+          questions, custom modules, and complicated upgrades.
+        </p>
+        <div className="mt-10">
+          <div className="flex gap-1">
+            <h2 className="leading-0 mb-0 px-6 py-4 bg-[#ff6624] rounded-t-md">
+              Private Ticket
+            </h2>
+            <div className="flex gap-1 items-end">
+              <Link
+                className="px-6 py-2 bg-[rgba(0,127,230,0.2)]  rounded-t-md text-red-600 font-bold hover:text-red-600 hover:bg-[#058ffe] dark:bg-[rgba(0,105,190,0.25)] dark:hover:hover:bg-[#058ffe]"
+                href="/support/premium"
+              >
+                Urgent
+              </Link>
+              <Link
+                className="px-6 py-2 bg-[rgba(0,127,230,0.2)]  rounded-t-md text-black dark:text-white font-bold hover:text-white hover:bg-[#058ffe] dark:bg-[rgba(0,105,190,0.25)] dark:hover:hover:bg-[#058ffe]"
+                to="https://github.com/solspace/craft-freeform/"
+              >
+                Github
+              </Link>
+              <Link
+                className="px-6 py-2 bg-[rgba(0,127,230,0.2)]  rounded-t-md text-black dark:text-white font-bold hover:text-white hover:bg-[#058ffe] dark:bg-[rgba(0,105,190,0.25)] dark:hover:hover:bg-[#058ffe]"
+                to="https://craftcms.stackexchange.com/questions/tagged/solspace"
+              >
+                Stack Exchange
+              </Link>
+            </div>
+          </div>
+          <div className="mx-auto p-6 bg-[#181d27] text-white rounded-b-md rounded-tr-md border border-solid border-[#ff6624]">
+            <h2 className="text-2xl font-bold mb-4 text-gray-200">
+              Create a Support Ticket
+            </h2>
+            <p className="text-gray-200">
+              If you need assistance, have a question before making a purchase,
+              or simply want to get in touch, please fill out the form below.
+            </p>
+            <iframe
+              title="Support Form"
+              id="support-form"
+              src="https://support.solspace.com/software/support"
+              scrolling="yes"
+              height="850px"
+              width="100%"
+              frameBorder="0"
+            ></iframe>
+          </div>
+        </div>
+      </main>
+      <footer className="docusaurus-mt-lg docs-footer">
+        <Footer />
+      </footer>
+    </Layout>
+  );
+};
+
+export default Support;
